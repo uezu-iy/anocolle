9d6c77a387336baf80c37dca0f75dfdb6f016d31
yutouezu <fyyargixi@gmail.com>
2023-04-17 01:27:59 +0900

    add:usersモデルのために新たなにルーティングしました

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'anomalys#index'
   devise_for :users
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :anomalys
   get "search_tag"=>"anomalys#search_tag"
