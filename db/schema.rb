02d0741f8633a7578e6aa01dbfe7f9b8e1f5d1dc
yutouezu <fyyargixi@gmail.com>
2023-04-18 03:35:38 +0900

    add:commentモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_12_041655) do
+ActiveRecord::Schema.define(version: 2023_04_17_072753) do
 
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
@@ -66,6 +66,16 @@ ActiveRecord::Schema.define(version: 2023_04_12_041655) do
     t.index ["tag_id"], name: "index_anomaly_tags_on_tag_id"
   end
 
+  create_table "comments", force: :cascade do |t|
+    t.text "comment_content"
+    t.integer "user_id", null: false
+    t.integer "anomaly_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["anomaly_id"], name: "index_comments_on_anomaly_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
   create_table "tags", force: :cascade do |t|
     t.string "tag_name"
     t.datetime "created_at", precision: 6, null: false
@@ -89,4 +99,6 @@ ActiveRecord::Schema.define(version: 2023_04_12_041655) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "anomaly_tags", "anomalies"
   add_foreign_key "anomaly_tags", "tags"
+  add_foreign_key "comments", "anomalies"
+  add_foreign_key "comments", "users"
 end
