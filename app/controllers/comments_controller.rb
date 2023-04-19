7b4919f7c3e14ad0043fdf7d698de3964fc79954
yutouezu <fyyargixi@gmail.com>
2023-04-20 03:00:27 +0900

    add:コメント削除機能の実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,24 @@
 class CommentsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def create
     @comment = current_user.comments.new(comment_params)
     if @comment.save
-      redirect_back(fallback_location: root_path)  #コメント送信後は、一つ前のページへリダイレクトさせる。
+      redirect_back(fallback_location: root_path)
     else
-      redirect_back(fallback_location: root_path)  #同上
+      redirect_back(fallback_location: root_path)
     end
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+      @comment.destroy
+      flash[:success] = "コメント削除完了"
+      redirect_to anomaly_path(@comment.anomaly_id)
   end
   
   private
   def comment_params
-    params.require(:comment).permit(:comment_content, :anomaly_id)  #formにてpost_idパラメータを送信して、コメントへpost_idを格納するようにする必要がある。
+    params.require(:comment).permit(:comment_content, :anomaly_id)
   end
 end
