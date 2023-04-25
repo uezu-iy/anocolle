6ec38eb193c5cf1c8bdd291c9770bf3b3fa4d20b
yutouezu <fyyargixi@gmail.com>
2023-04-25 09:21:11 +0900

    update:ルートパスを変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'anomalys#index'
+  root 'home#index'
   devise_for :users
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
