5c0006dbdca80d644e230d37a43626978c2b5292
yutouezu <fyyargixi@gmail.com>
2023-05-07 12:25:52 +0900

    add:tag_nameにユニーク制約を追加する為マイグレーションファイルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_17_072753) do
+ActiveRecord::Schema.define(version: 2023_05_07_023217) do
 
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
@@ -80,6 +80,7 @@ ActiveRecord::Schema.define(version: 2023_04_17_072753) do
     t.string "tag_name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["tag_name"], name: "index_tags_on_tag_name", unique: true
   end
 
   create_table "users", force: :cascade do |t|
