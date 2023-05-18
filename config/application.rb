3d3b06ede28b336afb77478848e15381bf2cf640
yutouezu <fyyargixi@gmail.com>
2023-05-19 00:33:01 +0900

    fix:rubocopでエラーが出た為不要な記述を削除

--- config/application.rb
+++ config/application.rb
@@ -18,10 +18,8 @@ module Anocolle
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.paths.add 'lib', eager_load: true
     config.time_zone = 'Tokyo'
     config.i18n.default_locale = :ja
-    config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
 
     config.generators do |g|
       g.test_framework :rspec,
